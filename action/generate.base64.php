02554637cf0fb085af4c4f56b84a18f8f7d1464b
Williham Totland <wito@eyego.no>
2012-01-23 10:47:10 +0100

    base64 fixes

--- action/generate.base64.php
+++ action/generate.base64.php
@@ -1,27 +1,9 @@
 <?php
 
-// function for base64 encode and decode
-function mybase64($array) {
-  
-  if ( $array['method'] == "encode" ) {
-    return base64_encode($array['userdata']);
-  } elseif ( $array['method'] == "decode" ) {
-    return base64_decode($array['userdata']);
-  } else {
-    die('This is not a method');
-  }
-  
+$data = $_POST['input'];
+
+if ($_POST['method'] == 'encode') {
+  echo base64_encode($data);
+} else {
+  echo base64_decode($data);
 }
-
-
-// setup array for base64 function 
-$array = array();
-
-// setup userdata and method variables sendt trough post
-$array['userdata'] = filter_var($_POST['input']);
-$array['method'] = filter_var($_POST['method']);
-
-// echo result for ajax
-echo mybase64($array);
-
-
