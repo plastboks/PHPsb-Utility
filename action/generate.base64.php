823d56c176b7dcd1727d27622e1cb2ed9461208f
Williham Totland <wito@eyego.no>
2012-01-23 11:19:56 +0100

    base64 chunk_split

--- action/generate.base64.php
+++ action/generate.base64.php
@@ -3,7 +3,7 @@
 $data = $_POST['input'];
 
 if ($_POST['method'] == 'encode') {
-  echo base64_encode($data);
+  echo chunk_split(base64_encode($data),45);
 } else {
   echo base64_decode($data);
 }
