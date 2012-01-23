36a8a49a062e1b7297caae7257f4e24790748d5d
Williham Totland <wito@eyego.no>
2012-01-23 10:57:21 +0100

    hash digest changes

--- action/generate.hash.php
+++ action/generate.hash.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 
 // function for generating sha1 hash, reason for this function is for the possibility of change later.
 function sha1crypt($arg) {
@@ -13,15 +12,7 @@ function sha256crypt($arg) {
 
 // function for generating md5 hash, the additional "$1$" is the prefix used to identify md5 hashes.
 function md5crypt($argument){
-  $jumble = md5(rand(1,100000000));
-  $salt = substr($jumble,0,8);
-  $return = crypt($argument, '$1$' . $salt);
-  return $return;
-}
-
-// function for base64 encode, reason for this function is for the possibility of change later.
-function base64crypt($arg){
-  return base64_encode($arg);
+  return hash('md5',$arg);
 }
 
 // function for generating des hash.
@@ -31,41 +22,22 @@ function descrypt($argument){
   return crypt($argument, $salt);
 }
 
-// receive user input, santesize them and put them into variables.
-$userinput = filter_var($_POST['userinput']);
-$cryptmethod = filter_var($_POST['cryptmethod']);
+$userinput = $_POST['userinput'];
+$cryptmethod = $_POST['cryptmethod'];
 
-// do not accept iserinput of more than 32 characters
 if ( strlen($userinput) > 32 ) {
   die("Max 32 characters in input");
 }
 
-// do some more checking of userinput. (never trust your users).
-switch ($cryptmethod) {
-  case "sha1crypt":
-    $genhash = "sha1crypt";
-    break;
-  case "sha256crypt":
-    $genhash = "sha256crypt";
-    break;
-  case "md5crypt":
-    $genhash = "md5crypt";
-    break;
-  case "base64crypt":
-    $genhash = "base64crypt";
-    break;
-  case "descrypt":
-    $genhash = "descrypt";
-    break;
-  default:
-   die("This is not a supported function");
+$methods = array(
+  'sha1crypt',
+  'sha256crypt',
+  'md5crypt',
+  'descrypt',
+);
+
+if (in_array($cryptmethod, $methods)) {
+  echo $cryptmethod($userinput);
+} else {
+  die('Unsupported method');
 }
-
-// run the function
-$generatedhash = $genhash($userinput);
-
-
-// echo result for ajax
-echo $generatedhash;
-
-
