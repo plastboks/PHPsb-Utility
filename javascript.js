bd63010d67a301b087445892b87745d497d08b6b
Williham Totland <wito@eyego.no>
2012-01-23 13:13:49 +0100

    js cleanup

--- javascript.js
+++ javascript.js
@@ -1,9 +1,4 @@
-// DOCUMENTREADY
-$(function() {
-
-
 function ajax_submit(form, output) {
-  
   var my_form = form;
   var data = my_form.serialize();
   var method = my_form.attr('method');
@@ -18,10 +13,9 @@ function ajax_submit(form, output) {
       output.html(response);
     }
   });
-
 }
 
-$('.genpassword').live('click', function(){ 
+$('.genpassword').live('click', function(){
   ajax_submit( $(this).closest('form'), $('.jq_genpassword_result') );
   return false;
 });
@@ -44,43 +38,4 @@ $('.genhash').live('click', function(){
 $('.genbase64').live('click', function(){
   ajax_submit( $(this).closest('form'), $('.jq_genbase64_result'));
   return false;
-});
-
-
-// No enter on forms 
-$('form').live('keydown', function(ev){
-  if(e.keyCode == 13){
-    return false;
-  }
-});
-
-
-// Detect change in input function
-function detect_change(input, min, max){
-
-	input.change(function(ev){
-    
-    // if input is lower than min value
-		if(input.val() < min){
-			input.val(min);
-		}
-
-    // if input it higher than max value
-		if(input.val() > max){
-			input.val(max)
-		}
-
-	});
-
-};
-
-detect_change($('#jq_input_qty'), 1, 32);
-detect_change($('#jq_input_num'), 6, 32);
-detect_change($('#jq_input_llnum'), 0, 16);
-detect_change($('#jq_input_lunum'), 0, 16);
-detect_change($('#jq_input_nnum'), 0, 16);
-detect_change($('#jq_input_pnum'), 0, 16);
-
-
-/* END DOCUMENTREADY */
 });
